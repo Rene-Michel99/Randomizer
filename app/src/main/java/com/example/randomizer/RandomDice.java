ccaa5b21521dd5cafe8c2f3d5fdca0184b5faa9c
Rene Michel <renemichel856@gmail.com>
2020-12-23 23:37:10 -0300

    Finalizacao do RandomDice

--- app/src/main/java/com/example/randomizer/RandomDice.java
+++ app/src/main/java/com/example/randomizer/RandomDice.java
@@ -4,15 +4,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.fragment.app.Fragment;
+
+import java.util.Random;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link RandomDice#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class RandomDice extends Fragment {
+public class RandomDice extends Fragment implements View.OnClickListener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -22,6 +26,9 @@ public class RandomDice extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private ImageView img_dice = null;
+    private Button btn = null;
 
     public RandomDice() {
         // Required empty public constructor
@@ -58,6 +65,49 @@ public class RandomDice extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_random_dice, container, false);
+        View view = inflater.inflate(R.layout.fragment_random_dice, container, false);
+
+        btn = (Button) view.findViewById(R.id.roll_dice);
+        img_dice = (ImageView) view.findViewById(R.id.dice);
+
+        btn.setOnClickListener(this);
+
+        return view;
+    }
+
+    private void change_dice(int num){
+        switch (num){
+            case 1:
+                img_dice.setImageResource(R.drawable.face1);
+                break;
+            case 2:
+                img_dice.setImageResource(R.drawable.face2);
+                break;
+            case 3:
+                img_dice.setImageResource(R.drawable.face3);
+                break;
+            case 4:
+                img_dice.setImageResource(R.drawable.face4);
+                break;
+            case 5:
+                img_dice.setImageResource(R.drawable.face5);
+                break;
+            case 6:
+                img_dice.setImageResource(R.drawable.face6);
+                break;
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == btn){
+            Random rand = new Random();
+
+            int min = 1, max = 6;
+
+            int num = rand.nextInt((max - min) + 1) + min;
+            change_dice(num);
+
+        }
     }
 }
